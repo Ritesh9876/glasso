522b0ab4c1019a76092b2d36588eee0c1ecc767e
Ritesh9876 <riteshmangdare@gmail.com>
2023-06-30 14:49:37 +0530

    basic structure update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import './Components/common.css'
 import ReactDOM from 'react-dom/client';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
